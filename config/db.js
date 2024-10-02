fa732cb1792171a1e01beaaff1a59c445c22c9bf
Luis Carlos <luiscbravo94@gmail.com>
2024-10-02 23:54:28 +0100

    added dbConnection function

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Product = require('../models/Product') 
+require('dotenv').config()
+
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Database has been connected correctly`);
+        
+    } catch (error) {
+        throw new Error ('There was a problem trying to connect with database')
+        
+    }
+    
+}
+
+module.exports(dbConnection)
