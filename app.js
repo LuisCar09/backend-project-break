2fc5bb56286d3af706787de2a9e04fe2d01fcdf1
Luis Carlos <luiscbravo94@gmail.com>
2024-10-03 00:55:59 +0100

    import dashboard routes to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 const path = require('path')
 const productRoutes= require('./routes/productRoutes')
-
+const dashboardRouters = require('./routes/authRoutes')
 const dbConnection = require('./config/db')
 dotenv.config()
 const PORT = process.env.PORT ?? 4500
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.use('/products',productRoutes)
-//app.use('/dashboard',)
+app.use('/dashboard',dashboardRouters)
 
 dbConnection()
 app.listen(PORT,()=>{
